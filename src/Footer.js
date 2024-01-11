493bcbf156be00f44af64afb86c45420e28af84c
Ahmad Muzakki <yitna.yuana@gmail.com>
2024-01-11 10:53:58 +0700

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<footer>
+			<p>© 2024 Nama Proyek Anda</p>
+		</footer>
+	);
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+	return (
+		<main>
+			<p>Selamat datang di proyek React.js sederhana!</p>
+		</main>
+	);
+}
+
+export default Main;
