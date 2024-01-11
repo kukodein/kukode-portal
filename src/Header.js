e8c1ae6ebee1568caa3b63349ea035798e53846c
Ahmad Muzakki <yitna.yuana@gmail.com>
2024-01-11 13:55:37 +0700

    update meta head

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 function Header() {
 	return (
 		<header>
+			<Helmet>
+				<title>Template React.js</title>
+				<meta name="robots" content="noindex" />
+			</Helmet>
 			<h1>Template React.js</h1>
 		</header>
 	);
